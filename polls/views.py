64c0009cbe6d0aa6b31b0d31d3f54358d6767bf5
TheMonist <rogerrosena@gmail.com>
2024-12-01 16:35:03 -0500

    adding migrations

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
+
+
+def details(request, question_id):
+    return HttpResponse("Youre looking at %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Your looking at the voting on questions %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
